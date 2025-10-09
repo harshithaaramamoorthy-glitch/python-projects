9750c5eabecc382412f18e2cbf7265ac3e34ac42
harshithaaramamoorthy-glitch <harshithaaramamoorthy@gmail.com>
2025-10-09 13:36:10 +0530

    9.10.25
    9.10.25

---
+++ vehicle.py
@@ -0,0 +1,42 @@
+import streamlit as st
+class vehicle:
+   def __init__(self,brand,model):
+    super().__init__(brand,ModuleNotFoundError)
+    self.brand=brand
+    self.model=model
+   def __init__(self,displaylight):
+    super().__init__(dispalylight)
+    self.displaylight
+class car(vehicle):
+   def __init__(play_music):
+    super().__init__(paly_music)
+class electriccar(car):
+   def __init__(self,lowcharge,highcharge):
+    super().__init__(lowcharge,highcharge)  
+    self.lowcharge=lowcharge
+    self.highcharge=highcharge
+class smartdevice:
+   def __init__(self,on,off):
+     super().__init__(on,off)
+     self.wifion
+     self.wifioff
+class smartcarautodrive(car,smartdevice):
+   def __init__(self,on,off):
+     super().__init__(on,off)
+     self.switchon
+     self.switchoff
+class bike(vehicle):
+   def __init__(bike):
+    super().__init__(bike)
+class  electricsmartcar(car):
+   def electricsmartcar(self,on,off):
+     super()._-init__(on,off)
+     self.on=on
+     self.off=on
+a=str(input("enter brand name :"))
+b=str(input("enter model name : "))
+   
+   
+   
+      
+     
